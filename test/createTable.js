fcee716886dc66be40b916d9412812a015f17b9d
Peng Xie <xiepeng.peng@gmail.com>
2012-01-18 21:15:58 +0000

    list tables

--- test/createTable.js
+++ test/createTable.js
@@ -2,8 +2,13 @@
  */
 
 
-var dynamoDB = require('../lib/dynamoDB');
+var dynamoDB = require('../lib/dynamoDB'),
+    auth = require('../lib/auth');
 require('fs').readFile('credentials.json', 'utf8', function (err, data) {
     if (err) { throw err; }
-    console.log(JSON.stringify(dynamoDB.DynamoDB(JSON.parse(data))));
+    var db = dynamoDB.DynamoDB(JSON.parse(data));
+    console.log(JSON.stringify(db));
+    db.listTables();
+    setTimeout(db.listTables, 2000);
+    
 });
