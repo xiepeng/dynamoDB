fe9d6bc2c819beb4cfb99ddf45f3fb4a1c1cbf89
Peng Xie <xiepeng.peng@gmail.com>
2012-01-20 16:37:59 +0000

    Update README

--- example.js
+++ example.js
@@ -4,27 +4,7 @@ var credentials = {AccessKeyId : "*Your Access Key**",
 
 var dynamoDB = require('./lib/dynamoDB').DynamoDB(credentials);
 
-
-dynamoDB.listTables({"ExclusiveStartTableName":"Aable1","Limit":3}, function(result) {
-    result.on('data', function(chunk){
-        console.log(""+chunk);
-    });
-});
-
-dynamoDB.deleteTable({"TableName":"Table1"}, function(result) {
-    result.on('data', function(chunk){
-        console.log(""+chunk);
-    });
-});
-
-
-dynamoDB.describeTable({"TableName":"Table1"}, function(result) {
-    result.on('data', function(chunk){
-        console.log(""+chunk);
-    });
-});
-
-
+/*
 dynamoDB.createTable(
 {"TableName":"Table1",
     "KeySchema":
@@ -37,32 +17,113 @@ dynamoDB.createTable(
         console.log(""+chunk);
     });
 });
+*/
 
+dynamoDB.listTables({}, function(result) {
+    result.on('data', function(chunk){
+        console.log(""+chunk);
+    });
+});
 
-dynamoDB.putItem(
-{"TableName":"Table1",
-    "Item":{
-        "Color":{"S":"white"},
-        "Name":{"S":"fancy vase"},
-        "Weight":{"N":"2"}
-    }
-}
-, function(result) {
+/*
+dynamoDB.deleteTable({"TableName":"Table1"}, function(result) {
+    result.on('data', function(chunk){
+        console.log(""+chunk);
+    });
+});
+*/
+
+dynamoDB.describeTable({"TableName":"Table1"}, function(result) {
     result.on('data', function(chunk){
         console.log(""+chunk);
     });
 });
 
 
+dynamoDB.putItem(
+    {"TableName":"Table1",
+        "Item":{
+            "Color" :{ "S":"white"},
+            "Name"  : {"S":"fancy vase"},
+            "Weight": {"N":"2"}
+        }
+    }
+, function(result) {
+        result.on('data', function(chunk){
+            console.log(""+chunk);
+        });
+});
+
+
+dynamoDB.putItem(
+    {"TableName":"Table1",
+        "Item":{
+            "Color" :{ "S":"blue"},
+            "Name"  : {"S":"vanilla bowl"},
+            "Weight": {"N":"5"}
+        }
+    }
+, function(result) {
+        result.on('data', function(chunk){
+            console.log(""+chunk);
+        });
+});
+
+
+dynamoDB.putItem(
+    {"TableName":"Table1",
+        "Item":{
+            "Color" :{ "S":"red"},
+            "Name"  : {"S":"sofa"},
+            "Weight": {"N":"300"}
+        }
+    }
+, function(result) {
+        result.on('data', function(chunk){
+            console.log(""+chunk);
+        });
+});
+
+
 dynamoDB.getItem(
-{"TableName":"Table1",
-    "Key": 
-        {"HashKeyElement": {"S":"white"},
-        "RangeKeyElement": {"N":"2"} 
-    },
-    "AttributesToGet":["Color","Weight", "Name"],
-    "ConsistentRead":true
-}
+    {"TableName":"Table1",
+        "Key":
+            {"HashKeyElement"   : {"S":"white"},
+            "RangeKeyElement"   : {"N":"2"}
+        },
+        "AttributesToGet"   : ["Color","Weight", "Name"],
+        "ConsistentRead"    : true
+    }, function(result) {
+        result.on('data', function(chunk){
+            console.log(""+chunk);
+        });
+});
+
+
+dynamoDB.batchGetItem(
+    {"RequestItems":
+        {"Table1": 
+            {"Keys": 
+                [{"HashKeyElement"  : {"S":"white"}, "RangeKeyElement":{"N":"2"}},
+                {"HashKeyElement"   : {"S":"blue"}, "RangeKeyElement":{"N":"5"}},
+                {"HashKeyElement"   : {"S":"red"}, "RangeKeyElement":{"N":"3"}}],  // you won't get sofa because it weighs 300.
+            "AttributesToGet":["Color", "Weight", "Name"]}
+        }
+    }
+, function(result) {
+    result.on('data', function(chunk){
+        console.log(""+chunk);
+    });
+});
+
+dynamoDB.scan(
+    {"TableName"    :"Table1",
+        "Limit"     : 2,
+        "ScanFilter":{
+            "Name"  :{"AttributeValueList":[{"S":"sofa"}],"ComparisonOperator":"EQ"}
+        },
+        "AttributesToGet":["Color", "Weight", "Name"]
+    }
 , function(result) {
     result.on('data', function(chunk){
         console.log(""+chunk);
